1d5ec3a25cbc5cfd6cab30c6d922635fb5042036
Natthakan Saeng-nil <110548452+Tanknam@users.noreply.github.com>
2022-09-19 23:36:04 +0800

    Update ThreeSum.py

--- ThreeSum.py
+++ ThreeSum.py
@@ -44,39 +44,3 @@ def threeSum(nums):
     return result
 
 threeSum([-2,0,0,2,2])
-
-twoSum([-1,0,1,2], 1)
-
-    j=0
-    for i in nums:
-        pair = twoSum(nums[j+1:], 0 - i)
-        j += 1
-        if pair != False:
-            for x in pair:
-                sol = [i,x[0],x[1]]
-                result.append(sol)
-
-
-a = [0,2,-2]
-b = [2,-2,0]
-set(a) == set(b)
-
-
-
-d = {'2':'5'}
-
-
-c = [1,1,2,4]
-c.remove(1)
-c
-
-c = [[2, 0, -2], [-2, 0, 2]]
-c[0] in c[1]
-
-set(c[0]
-d = {}
-for i,n in enumerate(c):
-    if set(n) in d:
-        d[set(n)] += 1
-    else:
-        d[set(n)] = 1
